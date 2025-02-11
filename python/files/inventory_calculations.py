d4dddc7ad47d4744419c363a217d7b0c5cf1e33b
aloysathekge <sathekgealoy@gmail.com>
2025-02-11 13:54:44 +0200

    left off at dict

--- python/files/inventory_calculations.py
+++ python/files/inventory_calculations.py
@@ -1,16 +1,30 @@
 import os
+
 print("Current working directory:", os.getcwd())
 
-def read_file(file):
 
+def read_file(file):
+    list_of_products = []
     with open(file) as f:
-        return f.read()
+        for line in f.readlines():
+            list_of_products.append(line)
+    return list_of_products
+
+
+def calculations(list_of_products: list):
+    products_dict = {}
+    for product in list_of_products:
+        name, quantity, price = product.split(",")
+        products_dict[name] = products_dict.get(name, 0) + (
+            int(quantity) * float(price)
+        )
+    return products_dict
 
 
 def main():
     file = "products.txt"
     result = read_file(file)
-    print(result)
+    print(calculations(result))
 
 
 if __name__ == "__main__":
