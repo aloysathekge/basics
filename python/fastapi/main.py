bda3d9cec25133f28b740da5e03d3e48253d0027
aloysathekge <sathekgealoy@gmail.com>
2025-07-21 19:53:30 +0200

    Add item retrieval and listing endpoints to FastAPI application

--- python/fastapi/main.py
+++ python/fastapi/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, HTTPException
 
 app = FastAPI()
 items = []
@@ -13,3 +13,17 @@ def root():
 def create_items(item: str = Body()):
     items.append(item)
     return items
+
+
+@app.get("/items/{item_id}")
+def get_items(item_id: int):
+    if item_id < len(items):
+        item = items[item_id]
+        return item
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+
+@app.get("/items")
+def list_items(limit: int = 10):
+    return items[0:limit]
