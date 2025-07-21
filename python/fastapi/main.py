d888623e0e14ed3409cbd7b0654f7b9c32ead0b4
aloysathekge <sathekgealoy@gmail.com>
2025-07-21 17:56:51 +0200

    Enhance FastAPI application with item creation endpoint and fix greeting response format

--- python/fastapi/main.py
+++ python/fastapi/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
+items = []
 
 
 @app.get("/")
 def root():
-    return {"Hello: World"}
+    return {"Hello": "World"}
+
+
+@app.post("/items")
+def create_items(item: str = Body()):
+    items.append(item)
+    return items
